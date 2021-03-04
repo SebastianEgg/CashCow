0cc4ce5a44cee981d25e66b833e366081b08610d
SebastianEgg <bastian.egger@gmx.at>
2021-03-04 10:09:36 +0100

    Add project files.

---
+++ CashCow.Logic/Entities/CashCow/Payment.cs
@@ -0,0 +1,32 @@
+﻿using CashCow.Contracts.Persistence.CashCow;
+using CommonBase.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CashCow.Logic.Entities.CashCow
+{
+    internal partial 
+        class Payment : VersionEntity, IPayment
+    {
+        public string Title { get; set; }
+        public int Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Payer { get; set; }
+        public string Zahlung { get; set; }
+
+        public void CopyProperties(IPayment other)
+        {
+            other.CheckArgument(nameof(other));
+
+            Id = other.Id;
+            RowVersion = other.RowVersion;
+            Title = other.Title;
+            Amount = other.Amount;
+            Date = other.Date;
+            Payer = other.Payer;
+            Zahlung = other.Zahlung;
+            
+        }
+    }
+}
---
+++ CashCow.Logic/Entities/VersionEntity.cs
@@ -0,0 +1,15 @@
+﻿//@BaseCode
+using CashCow.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashCow.Logic.Entities
+{
+    internal abstract partial class VersionEntity : IdentityEntity, IVersionable
+    {
+        public byte[] RowVersion { get; set; }
+    }
+}
